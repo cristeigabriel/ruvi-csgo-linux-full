5004396daa806ec783963c7f1a0ccfaeda074815
I swear I'm not an alcoholic <46060482+Vulgar1s@users.noreply.github.com>
2019-01-13 13:17:34 +0100

    👺
    added mouse delta stuff

--- src/framework/input/input.cc
+++ src/framework/input/input.cc
@@ -16,10 +16,18 @@ void input::on_paint( ) {
 	for ( int x = 0; x < 255; x++ )
 		id.keys[ x ] = csgo::input_system->is_button_down( e_button_code( x ) );
 
-	// get the cursor position
-	csgo::vgui_surface->surface_get_cursor_pos( id.cursor.x, id.cursor.y );
+	// cursor position stuff
+	point last_cursor_pos;// this is used for the cursor delta
+	csgo::vgui_surface->surface_get_cursor_pos( id.cursor.x, id.cursor.y ); //gets current mouse pos
+	// the point struct does not have a - operator and I am to lazy to add one rn, so it stays written like aids VVV
+	id.cursor_delta.x = id.cursor.x - last_cursor_pos.x; // sets the delta for x
+	id.cursor_delta.y = id.cursor.y - last_cursor_pos.y  // sets the delta for y
+	last_cursor_pos = id.cursor; //sets this value last
 }
-
+point get_cursor_delta() {
+	
+	return id.cursor_delta;
+}
 bool input::get_key_state( e_button_code key ) { // if the key is being held
 
 	return id.keys[ key ];
