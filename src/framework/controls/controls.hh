3bfe5784ed3b7f1ca61b0d551c1d3cc39e6a6c15
Otávio H <ufloody@gmail.com>
2019-01-13 09:28:08 -0200

    :japanese_goblin:
    Ffs.

--- src/framework/controls/controls.hh
+++ src/framework/controls/controls.hh
@@ -9,11 +9,7 @@
 #include "utils/color.hh"
 
 namespace ruvi {
+
     void groupbox(int x, int y, int width, int height, std::string name);
-<<<<<<< HEAD
     bool checkbox(int x, int y, bool* variable, std::string name);
 }
-=======
-	bool checkbox(std::string name, bool *var, int x, int y);
-}
->>>>>>> master
--- src/framework/input/input.hh
+++ src/framework/input/input.hh
@@ -13,17 +13,12 @@ struct input_data {
 	bool pressed_keys[256];
 	bool old_keys[256];
 	point cursor;
-	point cursor_ghost; // last frames cursor pos, used for getting cursor_delta
-	point cursor_delta;
-	bool can_click;
 };
 
 namespace input {
 
 	void on_paint();
 
-	bool get_can_click(); 
-	void set_can_click(bool state);
 	bool get_key_state(e_button_code key);
 	bool get_key_press(e_button_code key);
 	bool in_bounds(int x, int y, int x2, int y2, int width, int height);
