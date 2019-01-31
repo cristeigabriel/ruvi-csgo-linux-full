37d1222f219485512ef86f925a0f844169b8e3a0
Floody <iFloody@users.noreply.github.com>
2019-01-31 08:23:13 -0200

    :japanese_goblin:
    clean up

--- src/vector/vector2d.hh
+++ src/vector/vector2d.hh
@@ -14,11 +14,6 @@ typedef float vec_t;
 typedef float vec2_t[2];
 typedef float vec3_t[3];
 
-// definitions
-#define FLOAT32_NAN_BITS (unsigned long) 0x7FC00000
-#define FLOAT32_NAN bits_to_float (FLOAT32_NAN_BITS)
-#define VEC_T_NAN FLOAT32_NAN
-
 class vector2d {
 public:
 	float x, y;
@@ -41,13 +36,6 @@ public:
 
 inline vec_t bits_to_float( unsigned long i ) {
 	return *reinterpret_cast<vec_t*>(&i);
-}
-
-inline vector2d::vector2d( void ) {
-#ifdef _DEBUG
-	// Initialize to NAN to catch errors
-	x = y = VEC_T_NAN;
-#endif
 }
 
 inline vector2d::vector2d( vec_t X, vec_t Y ) {
